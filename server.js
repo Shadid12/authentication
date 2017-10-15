3b588a05047de96b5d9dba2a43a79c12e4136809
shadid12 <shadidhaque2012@gmail.com>
2017-10-15 14:02:13 -0400

    added all libs

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
 
+mongoose.connect(keys.mongoURI);
 const app = express()
 
 app.get('/', function (req, res) {
